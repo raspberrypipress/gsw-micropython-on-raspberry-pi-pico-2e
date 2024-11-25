d8fea34a080fa5587ba55846fb051841b806d17d
Brian Jepson <bjepson@gmail.com>
2024-11-25 16:08:52 -0500

    updated example for Pico 2

--- eg/ch05/Traffic_Lights.py
+++ eg/ch05/Traffic_Lights.py
@@ -1,30 +1,27 @@
 import machine
 import time
-import _thread
 
 led_red = machine.Pin(15, machine.Pin.OUT)
 led_amber = machine.Pin(14, machine.Pin.OUT)
 led_green = machine.Pin(13, machine.Pin.OUT)
-button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_UP)
 buzzer = machine.Pin(12, machine.Pin.OUT)
 
 button_pressed = False
 
-def button_reader_thread():
-    global button_pressed 
-    while True:
-        if button.value() == 1:
-            button_pressed = True
-        time.sleep(0.01)
+def btn_handler(pin):
+    global button_pressed
+    if not button_pressed:
+        button_pressed = True
 
-_thread.start_new_thread(button_reader_thread, ())
+button.irq(trigger=machine.Pin.IRQ_FALLING, handler=btn_handler)
 
 while True:
     if button_pressed == True:
         led_red.value(1)
-        for i in range(10):
+        for i in range(20):
             buzzer.value(1)
-            time.sleep(0.04) 
+            time.sleep(0.05) 
             buzzer.value(0)
             time.sleep(0.2)
         button_pressed = False
