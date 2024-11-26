c4b9cf915943aaa05b849095f7fca21620df0ae1
Brian Jepson <bjepson@gmail.com>
2024-11-26 09:03:57 -0500

    changed to external LED, utime->time

--- eg/ap03/PIO_PWM.py
+++ eg/ap03/PIO_PWM.py
@@ -1,6 +1,6 @@
 from rp2 import PIO, StateMachine, asm_pio
-from machine import Pin
-import utime
+import machine
+import time
 
 @asm_pio(set_init=PIO.OUT_LOW)
 def quarter_bright():
@@ -16,20 +16,20 @@ def half_bright():
 def full_bright():
     set(pins, 1)
 
-led = Pin("LED")
+led = machine.Pin(15, machine.Pin.OUT)
 sm1 = StateMachine(1, quarter_bright, freq=10000, set_base=led)
 sm2 = StateMachine(2, half_bright, freq=10000, set_base=led)
 sm3 = StateMachine(3, full_bright, freq=10000, set_base=led)
 
 while(True): 
     sm1.active(1)
-    utime.sleep(1)
+    time.sleep(1)
     sm1.active(0)
 
     sm2.active(1)
-    utime.sleep(1)
+    time.sleep(1)
     sm2.active(0)
 
     sm3.active(1)
-    utime.sleep(1)
+    time.sleep(1)
     sm3.active(0)
